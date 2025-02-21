c4dff9e213dc23d534e1d0047ec6f6f7ff6285df
jiyoungnim <cloundyon31@gmail.com>
2025-02-21 23:49:58 +0900

    🎉 first commit

---
+++ index.tsx
@@ -0,0 +1,22 @@
+// Welcome to the TypeScript Playground, this is a website
+// which gives you a chance to write, share and learn TypeScript.
+
+// You could think of it in three ways:
+//
+//  - A location to learn TypeScript where nothing can break
+//  - A place to experiment with TypeScript syntax, and share the URLs with others
+//  - A sandbox to experiment with different compiler features of TypeScript
+
+const anExampleVariable = "Hello World";
+console.log(anExampleVariable);
+
+// To learn more about the language, click above in "Examples" or "What's New".
+// Otherwise, get started by removing these comments and the world is your playground.
+
+const color: string = "red";
+const isRed: boolean = color === "red";
+const age: number = 25;
+
+const colors: string[] = ["red", "green", "blue"];
+const ages: number[] = [20, 30, 40];
+const areColorsRed: boolean[] = [true, false, false];
